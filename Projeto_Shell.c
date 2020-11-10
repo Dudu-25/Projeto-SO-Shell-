413370b8d0ff318871b3b2131782a28d3e92db2a
Eduardo Nunes <edununes726@gmail.com>
2020-11-10 19:12:07 -0300

    Shell 1.0

--- Projeto_Shell.c
+++ Projeto_Shell.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -8,116 +7,141 @@
 #include <stdio.h>
 #include <string.h>
 
-int getQntComandos(char comando[])
-{
-	int i,qntComandos=0;
-	i=0;
+int getQntComandos(char comando[]){
+	int qntVirgulas=0,i=0;
+	
+	//separando comandos por virgula
 	while(i!=strlen(comando)){
 		if(comando[i]==','){
-			qntComandos++;	
+			qntVirgulas++;	
 		}
 		i++;
 	}
-	return qntComandos+1;
+	
+	return qntVirgulas+1;
 }
 
-char* separarComandos(char comando[])
-{
-	int qntComandos=0,i,j;
-	int *tamanhoComando;
-	char **comandos;
+char* separar_executar_Comandos(char comando[]){
+	int qntComandos=0,i=0,j=0,posicao = 0,tamanhoComando=0,pid;
+	int *posicaoFimComando;
 	char diretorio[4096];
-	char *argv[3];
-	int pid;
-	qntComandos=getQntComandos(comando); //a quantidade de comandos Ã© iguala a quantidade de virgulas+1
-	tamanhoComando = (int*)malloc((qntComandos)*sizeof(int));
-	i=0;
-	j=0;
+	char *argv[3],*argg[5];
+	char **comandos;
+	char *verificar[1];
 	
+	//a quantidade de comandos é igual a quantidade de virgulas+1
+	qntComandos=getQntComandos(comando); 
+	posicaoFimComando = (int*)malloc((qntComandos)*sizeof(int));
+	
+	//guardando posicao em que os comandos terminam
 	while(i!=strlen(comando)){
 		if(comando[i]==','){
-			tamanhoComando[j]=i;
+			posicaoFimComando[j]=i;
 			j++;	
 		}
 		i++;
-	}
+	}	
+	posicaoFimComando[j]=strlen(comando);
 	
-	tamanhoComando[j]=strlen(comando);
 	comandos = (char*)malloc(qntComandos*sizeof(char));
-	
 	if(comandos==NULL){
 		printf("algo deu errado!");
 		exit(1);
 	}
+	
 	for(i=0;i<qntComandos;i++){
-		int a=0;
-		comandos[i]=(char*)malloc((tamanhoComando[i]-a)*sizeof(char));
-		a=tamanhoComando[i];
+	comandos[i]=(char*)malloc((posicaoFimComando[i])*sizeof(char));
 	}
 	
-	int posicao = 0,tamanho=0;
-	
+	//preencher matriz de comandos
 	for(i=0;i<qntComandos;i++){
-		tamanho=tamanhoComando[i]-tamanho;
-		for(j=0;j<tamanho;j++){
+		tamanhoComando=posicaoFimComando[i]-tamanhoComando;
+		for(j=0;j<tamanhoComando;j++){
+			//variavel posicao define a posicao do comando
 			comandos[i][j]=comando[posicao];
 			posicao++;
 		}
-		comandos[i][tamanho]='\0';	
-		posicao = tamanhoComando[i]+1;
-		tamanho= tamanhoComando[i]+1;
+		comandos[i][tamanhoComando]='\0';
+		//'+1' significa que estamos pulando a posicao da virgula	
+		posicao = posicaoFimComando[i]+1;
+		//tamanhocomando agora recebe o tamanho total do comando, inclusive a virgula
+		tamanhoComando= posicaoFimComando[i]+1;
 	}
+	
+	//executar o shell
 	for(i=0;i<qntComandos;i++){
-		
-		argv[0] = strtok(comandos[i], " ");
-        argv[1] = strtok(NULL, " ");
-        argv[2] = NULL;
-        	
-        if (strcmp(comandos[i],"") == 0){
-        printf("comando invalido\n");
-        }
-        	
-        else if (strcmp(argv[0], "pwd") == 0) {
-            getcwd(diretorio, 4096);
-            printf("%s\n", diretorio);
-        }
-        else if (strcmp(argv[0], "cd") == 0) {
-            if (chdir(argv[1]) != 0) {
-               	printf("Caminho invalido!\n");
-            }
-        }
-    	else {
-        	pid = fork();
-        	if (pid == 0) {
-           		if (execvp(argv[0], argv) == -1) {
-              		printf("Comando invalido\n");
-              		exit (0);
-            	}
-        	}
-        	else {
-           		wait();
-        	}
-    	}	
+		if (strcmp(comandos[i],"") == 0){
+			printf("comando invalido\n");
+		}
+		else{
+			argv[0] = strtok(comandos[i], " ");
+			if(strcmp(argv[0],"quit") == 0){	
+				return "quit";
+			}
+			else if(strcmp(argv[0], "grep") == 0) {
+				argg[2] = strtok(NULL, " ");
+				argg[3] = strtok(NULL, " ");
+				argg[4] = NULL;
+				argg[0] = "grep";
+				argg[1] = "-n";
+			}
+			else{
+				argv[1] = strtok(NULL, " ");
+				argv[2] = NULL;
+			}
+			if(strcmp(argv[0], "grep") == 0) {
+				pid = fork();
+			    	if (pid == 0) {
+		       		if (execvp(argg[0], argg) == -1) {
+			  			printf("Comando invalido\n");
+			  			exit (0);
+					}
+		    		}
+			}
+			
+		 	else if (strcmp(argv[0], "pwd") == 0) {
+		    		getcwd(diretorio, 4096);
+		    		printf("%s\n", diretorio);
+			}
+			else if (strcmp(argv[0], "cd") == 0) {
+		    		if (chdir(argv[1]) != 0) {
+		       		printf("Caminho invalido!\n");
+		       	}
+		    	}
+			else {
+		    		pid = fork();
+		    		if (pid == 0) {
+		       		if (execvp(argv[0], argv) == -1) {
+			  			printf("Comando invalido\n");
+			  			exit (0);
+					}
+		    		}
+		    		else {
+		       		wait();
+		    		}
+			}
+		}
+        		
 	}
-	free(tamanhoComando);	
+	free(posicaoFimComando);	
 	for(i=0;i<qntComandos;i++){
 		free(comandos[i]);
 	}
 	free(comandos);
-	return 0;
+	return "continuar o shell";
 }
 
 int main(){
-	
 	char comando[512];
-	printf("digite o comando\n");
-	fgets(comando,511,stdin);
-	comando[strlen(comando)-1]=0;
+	strcpy(comando ,"");
 	while(strcmp(comando,"quit")!=0){
-		separarComandos(comando);
-		printf("digite o comando\n");
+		sleep(0.5);
+		printf("Shell> ");
 		fgets(comando,511,stdin);
 		comando[strlen(comando)-1]=0;
+		strcpy(comando ,separar_executar_Comandos(comando));
+		//faz o programa esperar o termino da execucao da funçao acima
+		sleep(0.5);
 	}
 	return 0;
 }
---
+++ README.txt
@@ -0,0 +1,29 @@
+﻿Rômulo César de Freitas Monteiro - 3932
+Victor Paiva de Castro - 5973
+Eduardo Nunes de Oliveira - 6021
+
+
+Exemplos de como utilizar:
+
+
+Shell> ls -ll, pwd
+Shell> grep foo arquivo.txt
+Shell> cd diretório
+Shell> touch teste
+Shell> quit
+
+
+Bugs conhecidos ou problemas  
+
+
+* Caso ocorra da entrada ser “ ,,ls ” o programa entenderá que entre as vírgulas terá um comando, assim tratando que esse comando é um comando invalido.
+Ex: Shell> ,,ls
+comando invalido
+comando invalido
+saída do ls
+
+
+* Após a entrada de comandos seguido de um quit, o programa irá executar todos os comandos que estiverem antes do quit, assim após a execução dos comandos o quit será executado e efetuando a saída do programa. Ex: Shell> ls, pwd, quit.
+
+
+* Caso aconteça a entrada de um comando seguida de mais de um sub comando (Ex: ls -ll -la) somente o primeiro sub comando será executado (Ex: ls -ll).
